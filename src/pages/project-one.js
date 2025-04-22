247675de044c2c312b3b8de7afa06832968ff60e
Aisha Salimi <167572767+AishaSalimi@users.noreply.github.com>
2025-04-22 13:13:05 +0430

    polished

---
+++ src/pages/project-one.js
@@ -0,0 +1,95 @@
+import { Yellowtail } from "next/font/google";
+import { IoIosStar } from "react-icons/io";
+import { IoIosStarHalf } from "react-icons/io";
+import { BiSolidPencil } from "react-icons/bi";
+export default function ProjectOne() {
+  return (
+    <div className="bg-[#bec1d9] w-full h-screen flex items-center justify-center">
+      <div className="relative rounded-4xl bg-white w-[500px] h-[650px] p-6">
+        <p className="font-extrabold text-2xl">Whisk & Ladle Bristo</p>
+        <div className="flex items-center pb-2 pr-1">
+          <p>4.5</p>
+          <IoIosStar className="text-amber-300 text-3xl" />
+          <IoIosStar className="text-amber-300 text-3xl" />
+          <IoIosStar className="text-amber-300 text-3xl" />
+          <IoIosStar className="text-amber-300 text-3xl" />
+          <IoIosStarHalf className="text-amber-300 text-3xl" />
+          <p>(231 reviews)</p>
+        </div>
+        <div className="flex">
+          <div className="w-[350px] h-52">
+            <img
+              className="w-[350px] h-52 rounded-md"
+              alt=""
+              src="/images/rest.jpg"
+            />
+          </div>
+          <div className="flex flex-col gap-4 h-52 ml-3">
+            <img
+              className="h-24 object-cover rounded-md"
+              alt=""
+              src="/images/chef.jpg"
+            />
+            <img
+              className="h-24 object-cover rounded-md"
+              alt=""
+              src="/images/cafe.jpg"
+            />
+          </div>
+        </div>
+        <hr className="border-[#bec1d9] my-4"></hr>
+        <div className="space-y-2">
+          <p className="font-bold flex justify-center py-2">Customer Review</p>
+          <div className="bg-slate-200 rounded-3xl h-12 w-full flex items-center justify-center">
+            <IoIosStar className="text-amber-300 text-3xl" />
+            <IoIosStar className="text-amber-300 text-3xl" />
+            <IoIosStar className="text-amber-300 text-3xl" />
+            <IoIosStar className="text-amber-300 text-3xl" />
+            <IoIosStarHalf className="text-amber-300 text-3xl" />
+            <p> 4.5 out of 5</p>
+          </div>
+          <div className="flex items-center">
+            <span className="w-4 text-sm text-gray-700">5</span>
+            <div className="w-full bg-gray-200 rounded-full h-3">
+              <div className="bg-yellow-400 h-3 w-[70%] rounded-full"></div>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <span className="w-4 text-sm text-gray-700">4</span>
+            <div className="w-full bg-gray-200 rounded-full h-3">
+              <div className="bg-yellow-400 h-3 w-[85%] rounded-full"></div>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <span className="w-4 text-sm text-gray-700">3</span>
+            <div className="w-full bg-gray-200 rounded-full h-3">
+              <div className="bg-yellow-400 h-3 w-[60%] rounded-full"></div>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <span className="w-4 text-sm text-gray-700">2</span>
+            <div className="w-full bg-gray-200 rounded-full h-3">
+              <div className="bg-yellow-400 h-3 w-[65%] rounded-full"></div>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <span className="w-4 text-sm text-gray-700">1</span>
+            <div className="w-full bg-gray-200 rounded-full h-3">
+              <div className="bg-yellow-400 h-3 w-[20%] rounded-full"></div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="w-[200px] h-8 bg-white border border-black rounded-full flex items-center justify-center gap-1">
+              <BiSolidPencil className="text-black text-1xl" />
+              <p>Write Opinion</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/project-two.js
@@ -0,0 +1,83 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { FaQuoteRight } from "react-icons/fa";
+import { FaCircle } from "react-icons/fa";
+export default function ProjectTwo() {
+  return (
+    <div className="w-full 2xl:h-screen md:h-min-screen bg-[#363742]">
+      <div className="flex flex-col items-center justify-center pt-30 pb-15">
+        <p className="text-white text-[40px]">Do not take our word for it.</p>
+        <p className="text-4xl font-bold bg-gradient-to-r from-[#03de27] via-[#0fc9a7] to-[#88e5ff] bg-clip-text text-transparent">
+          Trust our customers
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center items-center gap-7 px-20">
+        <IoIosArrowBack className="hidden xl:block text-3xl text-amber-50 bg-[#54566b] rounded-sm" />
+        <div className="relative w-[350px] h-[450px] bg-[#262730] rounded-2xl p-4">
+          <div className="flex items-center justify-between">
+            <FaQuoteRight className="text-5xl text-[#54566b] m-4" />
+            <img
+              className="m-2 h-20 w-auto object-contain rounded-bl-full border border-[#03de27]"
+              alt="pic"
+              src="/images/p1.jpeg"
+            />
+          </div>
+          <p className=" text-white text-[20px] p-5">
+            This project management app is a game-changerl it's like having a
+            virtual project manager by your side, keeping everything organized
+            and on track. I've never felt more in control of my projects.
+          </p>
+          <p className="text-white pt-4 px-5">Carmen Walmsley</p>
+          <p className="text-[#54566b] text-[13px] px-5">
+            Head of Community @ByteBlast Solutions
+          </p>
+        </div>
+        <div className="relative w-[350px] h-[450px] bg-[#262730] rounded-2xl p-4">
+          <div className="flex items-center justify-between">
+            <FaQuoteRight className="text-5xl text-[#54566b] m-4" />
+            <img
+              className="m-2 h-20 w-auto object-contain rounded-bl-full border border-[#03de27]"
+              alt="pic"
+              src="/images/p2.jpeg"
+            />
+          </div>
+          <p className=" text-white text-[20px] p-5">
+            With this app, I've finally found the perfect solution for keeping
+            my team aligned and productive. From task assignment to progress
+            tracking, it covers all the bases and then some.
+          </p>
+          <p className="text-white pt-4 px-5">Joe Price</p>
+          <p className="text-[#54566b] text-[13px] px-5">
+            Business Analyst @Joe C. Price
+          </p>
+        </div>
+        <div className="relative w-[350px] h-[450px] bg-[#262730] rounded-2xl p-4">
+          <div className="flex items-center justify-between">
+            <FaQuoteRight className="text-5xl text-[#54566b] m-4" />
+            <img
+              className="m-2 h-20 w-auto object-contain rounded-bl-full border border-[#03de27]"
+              alt="pic"
+              src="/images/p3.jpeg"
+            />
+          </div>
+          <p className=" text-white text-[20px] p-5">
+            I've tried countless project management tools, but this app takes
+            the cake. It's intuitive, efficient, and packed with features that
+            make managing complex projects a breeze.
+          </p>
+          <p className="text-white pt-4 px-5">Kevin Dandrea</p>
+          <p className="text-[#54566b] text-[13px] px-5">
+            Project Manager @DataDynamo Solutions
+          </p>
+        </div>
+        <IoIosArrowForward className="hidden xl:block text-3xl text-amber-50 bg-[#54566b] rounded-sm" />
+      </div>
+      <div className="flex justify-center items-center gap-3 py-7">
+        <FaCircle className="text-[#03de27] hidden xl:block" />
+        <FaCircle className="text-[#262730] hidden xl:block" />
+        <FaCircle className="text-[#262730] hidden xl:block" />
+        <FaCircle className="text-[#262730] hidden xl:block" />
+      </div>
+    </div>
+  );
+}
